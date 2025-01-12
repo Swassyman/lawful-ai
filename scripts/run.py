2d908293779ad97750fb11b535f7963eb715d8ab
Swassyman <swassyman2005@gmail.com>
2025-01-12 09:46:24 +0530

    some edits

--- scripts/run.py
+++ scripts/run.py
@@ -8,5 +8,5 @@ genai.configure(api_key="AIzaSyBok_QsXHhwecpfe53v8onBjzFCRWyTM2A")
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Act as a lawyer and first give related laws and sections in India and any advice on it and dont say As an AI, I cannot provide legal advice." + user_input)
 
-with open("../data/response.txt", "w") as response_file:
+with open("/data/response.txt", "w") as response_file:
     response_file.write(response)
