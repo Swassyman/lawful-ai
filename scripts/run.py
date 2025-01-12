c21725f6e00b57ade5029058926b8041e04e460e
Swassyman <swassyman2005@gmail.com>
2025-01-12 06:29:26 +0530

    rearranged files and added better script

--- scripts/run.py
+++ scripts/run.py
@@ -1,23 +1,12 @@
+import sys
 import google.generativeai as genai
 import json
 
-with open("../data/prompt_response.json", "r") as file:
-    data = json.load(file)
+user_input = sys.argv[1]
 
-user_input=data['prompt']
 genai.configure(api_key="AIzaSyBok_QsXHhwecpfe53v8onBjzFCRWyTM2A")  
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Act as a lawyer and first give related laws and sections in India and any advice on it and dont say As an AI, I cannot provide legal advice." + user_input)
 
-
-file_path = "../data/prompt_response.json"
-
-# Read, update, and overwrite the file
-with open(file_path, "r+") as file:
-    data = json.load(file)  # Load existing JSON data
-    data["response"] = response.text  
-    file.seek(0)  # Move cursor to the beginning of the file
-    json.dump(data, file, indent=4)  # Write updated JSON
-    file.truncate()  # Remove any remaining old content
-
-
+with open("../data/response.txt", "w") as response_file:
+    response_file.write(response)
